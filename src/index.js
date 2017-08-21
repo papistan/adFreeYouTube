5fc9ff073eb85a51e73e90a393e4c7fc5fd02e90
papistan <jpapisan@gmail.com>
2017-08-21 15:14:40 -0700

    Add jsx to video list items

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-const API_KEY = ;
+
+const API_KEY = '';
 
 class App extends Component {
 	constructor(props) {
